6fb57b320557f5905dca08786e5ef9a57b292e5f
Renso7 <129785454+Renso7@users.noreply.github.com>
2024-03-29 13:46:12 -0500

    Add files via upload
    nada

---
+++ ATL-Academy_JavaScript/01script.js
@@ -0,0 +1,9 @@
+var Nombre = "Lucas";
+Nombre = "Jose";
+
+var Edad = "30";
+var Telefono = "964 123 456"
+
+var MensajeFinal = "Nombre: " + Nombre + ". Edad " + Edad + ". Telefono " + Telefono;
+
+alert (MensajeFinal);
